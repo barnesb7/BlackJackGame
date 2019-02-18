456899f686df24ea5288d48c5e1e0e197eac07e8
Brian B <brianb@MacBook-Pro-4.local>
2019-02-18 10:44:48 -0500

    Adds CardHand class with constructor and totalScore

--- src/main/java/CardHand.java
+++ src/main/java/CardHand.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CardHand {
+
+    private int totalScore;
+
+    private ArrayList<Card> hand;
+
+    public CardHand(ArrayList<Card> twoCardsFromDealer){
+        this.hand = new ArrayList<>(Arrays.asList(twoCardsFromDealer.get(0), twoCardsFromDealer.get(1)));
+    }
+
+//    public void sumHandTotalScore(){
+//
+////        for(Card card: hand){
+////            totalScore += Integer.parseInt(card.getRank());
+////        }
+//
+//    }
 
 
 }
