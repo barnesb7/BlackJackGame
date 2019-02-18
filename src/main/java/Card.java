2dc22a1ada03af03518b1bbc3dacda74f4463fd6
Brian B <brianb@MacBook-Pro-4.local>
2019-02-18 10:32:15 -0500

    Added Card class with constructor, getters, setters, and override for toString

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,31 @@
 public class Card {
+
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + rank + " of " + suit + "]";
+    }
 }
